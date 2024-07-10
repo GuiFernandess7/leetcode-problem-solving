1ebac8ad19670d90db67de580d3a05dae6551443
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-07-10 16:06:08 -0300

    feat: Rust code added

--- bestTimeToButStockI/rust_solution/src/main.rs
+++ bestTimeToButStockI/rust_solution/src/main.rs
@@ -1,3 +1,22 @@
+fn maxProfit(prices: &mut Vec<i32>) -> i32{
+    let mut min_price: i32 = std::i32::MAX;
+    let mut max_price: i32 = 0;
+
+    for price in prices.iter(){
+        if *price < min_price {
+            min_price = *price
+        }
+
+        if *price - min_price > max_price {
+            max_price = *price - min_price
+        }
+    }
+
+    max_price
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut prices = Vec::from([7,1,5,3,6,4]);
+    let mut result = maxProfit(&mut prices);
+    println!("{}", result);
 }
