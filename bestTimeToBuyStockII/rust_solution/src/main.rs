380bc9f3ccbae2ef0b5398a128ad5f3113cccc06
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-07-17 14:48:18 -0300

    feat: rust solution added

--- bestTimeToBuyStockII/rust_solution/src/main.rs
+++ bestTimeToBuyStockII/rust_solution/src/main.rs
@@ -1,3 +1,17 @@
+fn maxProfit(prices: &mut Vec<i32>) -> i32{
+    let mut total = 0;
+
+    for i in 0..prices.len() - 1{
+        if prices[i + 1] - prices[i] > 0 {
+            total += prices[i + 1] - prices[i]
+        }
+    }
+
+    return total;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut arr = vec![7,1,5,3,6,4];
+    let result = maxProfit(&mut arr);
+    println!("{}", result);
 }
